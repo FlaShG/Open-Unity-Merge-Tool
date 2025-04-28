7dc8ad5576cbc496ed6d0639854dad29124a230e
Sascha Graeff <sascha@blackicegames.de>
2025-04-28 19:53:39 +0200

    Fix rich text formatting

--- Editor/Merge Actions/MergeActionOurComponent.cs
+++ Editor/Merge Actions/MergeActionOurComponent.cs
@@ -8,7 +8,7 @@ namespace ThirteenPixels.OpenUnityMergeTool
     /// </summary>
     internal class MergeActionOurComponent : MergeActionComponent
     {
-        public override string Title => $"A <b>{componentType.Name}<b> component has been deleted on their branch.";
+        public override string Title => $"A <b>{componentType.Name}</b> component has been deleted on their branch.";
         public override string ApplyOursButtonLabel => "Keep";
         public override string ApplyTheirsButtonLabel => "Remove";
         public override bool IsUsingOurs => componentStays;
