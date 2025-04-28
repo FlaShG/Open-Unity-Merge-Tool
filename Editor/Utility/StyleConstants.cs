d4209c9795ae8be3d7f4f2d551a0024d787d4fba
Sascha Graeff <sascha@blackicegames.de>
2025-04-28 20:43:52 +0200

    Improve color constants

--- Editor/Utility/StyleConstants.cs
+++ Editor/Utility/StyleConstants.cs
@@ -5,9 +5,9 @@ namespace ThirteenPixels.OpenUnityMergeTool
 
     internal static class StyleConstants
     {
-        public static readonly Color UnmergedColor = new Color(0.4f, 0.2f, 0.2f);
-        public static readonly Color AutomergedColor = new Color(0.2f, 0.35f, 0.35f);
-        public static readonly Color MergedColor = new Color(0.2f, 0.4f, 0.2f);
+        public static readonly Color UnmergedColor = new Color(0.4f, 0.25f, 0.25f);
+        public static readonly Color AutomergedColor = new Color(0.25f, 0.35f, 0.35f);
+        public static readonly Color MergedColor = new Color(0.25f, 0.4f, 0.25f);
         // (0.3f, 0.7f, 1f) would be much nicer, but that collides with the default :active color
         public static readonly Color PrefabColor = new Color(0f, 0f, 0.4f);
 
