6bdebdc234994384f661da68d36025202d97d6ee
Sascha Graeff <sascha@blackicegames.de>
2025-05-03 21:16:21 +0200

    Improve merge tab layout and style

--- Editor/UI/Tabs/MergeTab.cs
+++ Editor/UI/Tabs/MergeTab.cs
@@ -1,6 +1,7 @@
 namespace ThirteenPixels.OpenUnityMergeTool
 {
     using UnityEditor;
+    using UnityEngine;
     using UnityEngine.UIElements;
 
     internal class MergeTab : MergeToolTab
@@ -10,7 +11,7 @@ namespace ThirteenPixels.OpenUnityMergeTool
 
         private VisualElement mergeUI;
         private VisualElement multipleObjectsUI;
-        private Button gameObjectButton;
+        private Label gameObjectLabel;
         private ScrollView scrollView;
         private ProgressBar progressBar;
         private Button finishButton;
@@ -76,16 +77,21 @@ namespace ThirteenPixels.OpenUnityMergeTool
             var line = new HorizontalLayout();
             line.style.flexShrink = 0;
             line.style.height = 30;
+            line.style.backgroundColor = new Color(0f, 0f, 0f, 0.3f);
+
+            var gameObjectButton = new Button(() => currentContainer.TargetGameObject.Highlight());
+            line.Add(gameObjectButton);
 
             var icon = new Image();
             icon.image = StyleConstants.Icons.GameObject;
             icon.style.width = 22;
             icon.style.height = 22;
-            icon.style.marginTop = 4;
-            line.Add(icon);
+            gameObjectButton.Add(icon);
 
-            gameObjectButton = new Button(() => currentContainer.TargetGameObject.Highlight());
-            line.Add(gameObjectButton);
+            gameObjectLabel = new Label();
+            gameObjectLabel.style.fontSize = 18;
+            gameObjectLabel.style.marginTop = 4;
+            line.Add(gameObjectLabel);
 
             line.Add(new HorizontalSpacer());
 
@@ -143,7 +149,7 @@ namespace ThirteenPixels.OpenUnityMergeTool
             if (container == currentContainer) return;
 
             currentContainer = container;
-            gameObjectButton.text = currentContainer.TargetGameObject.name;
+            gameObjectLabel.text = currentContainer.TargetGameObject.name;
             scrollView.Clear();
             foreach (var mergeAction in currentContainer.MergeActions)
             {
